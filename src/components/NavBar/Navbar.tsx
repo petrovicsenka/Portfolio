5aa3b599084ba275ab033f4e4784aca7d0e4080c
Senka Petrovic <petrovicsenka@outlook.com>
2024-06-24 12:02:38 -0700

    Updated links

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -12,18 +12,17 @@ const Navbar = () => {
         <Link id='section1' activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>
           Home
         </Link>
-        <Link id='section2' activeClass='active' to='about' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>
+        <Link id='section2' activeClass='active' to='about' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>
           About
         </Link>
         {/* to update after other sections are created: */}
-        {/* <Link id='section3' activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>
+        {/* <Link id='section3' activeClass='active' to='intro' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>
           Portfolio
         </Link> */}
-        <Link id='section4' activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>
-          Contact
-        </Link>
       </div>
-      <button className='desktopMenuBtn'>
+      <button className='desktopMenuBtn' onClick={() => {
+        document.getElementById('contact')?.scrollIntoView({behavior: 'smooth'});
+      }}>
         <img src={contactImg} alt='' className='desktopMenuImg' />
         Contact me
       </button>
