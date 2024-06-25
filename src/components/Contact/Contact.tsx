7d61c7aed7d4d9c4c6299da6a9141aeadf78b024
Senka Petrovic <petrovicsenka@outlook.com>
2024-06-25 15:00:02 -0700

    Added required fields to form and added links for linkedin and github

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -30,13 +30,17 @@ const Contact = () => {
       <h1 className='contactPageTitle'>Contact me</h1>
       <span className='contactDesc'>Please, fill out the form below to discuss any work opportunities.</span>
       <form className='contactForm' ref={form} onSubmit={sendEmail}>
-        <input type="text" className="name" placeholder='Your name' name='from_name' />
-        <input type="email" className="email" placeholder='Your email' name='from_email' />
-        <textarea className='msg' name='message' rows={5} placeholder='Your message'></textarea>
+        <input type="text" className="name" placeholder='Your name' name='from_name' required/>
+        <input type="email" className="email" placeholder='Your email' name='from_email' required />
+        <textarea className='msg' name='message' rows={5} placeholder='Your message' required ></textarea>
         <button type='submit' value='Send' className='submitBtn'>Submit</button>
         <div className="links">
+        <a href="https://github.com/petrovicsenka" target="_blank" rel="noopener noreferrer">
           <img src={github} alt="Github" className="link" />
+        </a>
+        <a href="https://linkedin.com/in/senka-petrovi%C4%87-95774b1b9/" target="_blank" rel="noopener noreferrer">
           <img src={linkedin} alt="Linkedin" className="link" />
+        </a>
         </div>
       </form>
     </section>
