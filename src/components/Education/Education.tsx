514f5b46debdae2b73faf75b293d4f4f9f47a699
Senka Petrovic <petrovicsenka@outlook.com>
2024-06-28 14:08:00 -0700

    Updated text

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -10,7 +10,7 @@ const Education = () => {
           <h2 className='educationText'>
             Faculty of Organizational Sciences, University of Belgrade
           </h2>
-          <p>2019-2020</p>
+          <p>2019 - 2020</p>
           <p>Master of Science in Organizational Sciences</p>
           <p>Department of Information Systems and Technologies</p>
         </div>
@@ -18,13 +18,13 @@ const Education = () => {
           <h2 className='educationText'>
             Faculty of Organizational Sciences, University of Belgrade
           </h2>
-          <p>2014-2019</p>
+          <p>2014 - 2019</p>
           <p>Bachelor of Science in Organizational Sciences</p>
           <p>Department of Information Systems and Technologies</p>
         </div>
         <div className='educationItem'>
           <h2 className='educationText'>Zemun Gymnasium, Belgrade</h2>
-          <p>2010-2014</p>
+          <p>2010 - 2014</p>
           <p>Department of Natural Sciences and Mathematics</p>
         </div>
       </div>
