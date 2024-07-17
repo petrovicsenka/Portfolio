9218e9e5e751e658f5c0d1bdd914e203f36949d3
Senka Petrovic <petrovicsenka@outlook.com>
2024-07-17 16:58:15 -0700

    Fixed offset for Learn-more button with lodash debounce

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,8 +3,39 @@ import bg from '../../assets/png/image.png';
 import { LearnMore } from '../../assets/svg/Learn-more.tsx';
 import { Link } from 'react-scroll';
 import Button from '../Button/Button';
+import { useState, useEffect } from 'react';
+import debounce from 'lodash/debounce';
+
+const offsets = {
+  small: 550,
+  medium: 450,
+  xmedium: 140,
+  slarge: 100,
+  large: 120,
+  xlarge: 222,
+};
+
+const getOffset = (width: number) => {
+  if (width < 450) return offsets.small;
+  if (width < 840) return offsets.medium;
+  if (width < 1000) return offsets.slarge;
+  if (width < 1280) return offsets.xmedium;
+  if (width < 1560) return offsets.large;
+  return offsets.xlarge;
+};
 
 const Intro = () => {
+  const [offset, setOffset] = useState(getOffset(window.innerWidth));
+
+  useEffect(() => {
+    const updateOffset = debounce(() => {
+      setOffset(getOffset(window.innerWidth));
+    }, 200);
+
+    window.addEventListener('resize', updateOffset);
+    return () => window.removeEventListener('resize', updateOffset);
+  }, []);
+
   return (
     <section id='intro'>
       <div className='introContent'>
@@ -24,7 +55,7 @@ const Intro = () => {
           to='learnMore'
           smooth={true}
           duration={500}
-          offset={222}
+          offset={offset}
           className='button'
         >
           <Button>
